96a6a9266efd20543422ad6fe2798c5814d1e901
Vittor Faria <vittorfpereira@gmail.com>
2020-04-10 15:36:09 -0300

    Creating module to check if a point is inside a city.

--- app/model/city_checker.py
+++ app/model/city_checker.py
@@ -5,8 +5,8 @@ from shapely.wkt import load
 class CityChecker:
 
     def __init__(self, file):
-        self.city_limits = load(file)
+        self.city_limits = load(open(file, 'r'))
 
-    def is_inside_city(self, lat, lon):
-        point = Point(lat, lon)
+    def is_inside(self, lat=None, lng=None):
+        point = Point(lng, lat)
         return self.city_limits.contains(point)
